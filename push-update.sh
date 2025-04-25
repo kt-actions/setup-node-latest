05cf7e0fc736afa2f1df2da5881e23052fd960c5
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-25 09:26:04 +0200

    update

--- push-update.sh
+++ push-update.sh
@@ -5,9 +5,12 @@ COMMIT_MESSAGE=${1:-"update"}
 # Second parameter is a major tag, defaults to "v1"
 MAJOR_TAG=${2:-"v1"}
 
+# Push the changes to the remote repository
 git add .
 git commit -m "$COMMIT_MESSAGE"
 git push
 
+# Force push the tag to the remote repository
+# This is necessary if the tag already exists
 git tag -af "$MAJOR_TAG" -m "$COMMIT_MESSAGE"
-git push origin "$MAJOR_TAG" --force
+git push --force origin "$MAJOR_TAG"
